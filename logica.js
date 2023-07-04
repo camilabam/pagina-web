e7f481cfdccdccb9c4fcae9b8fcc1da4e4a605b1
camilabam <137842735+camilabam@users.noreply.github.com>
2023-07-04 10:10:55 -0300

    Add files via upload

---
+++ logica.js
@@ -0,0 +1,14 @@
+let NombreDeLaVariable = "Hola mundo";
+//Comentario en js, primer variable let
+
+function nombreFuncion (parametro1, parametro2){
+
+let total = parametro1 + parametro2;
+
+return total;
+
+}
+
+let suma = nombreFuncion (2 ,2 )
+
+alert(suma)
